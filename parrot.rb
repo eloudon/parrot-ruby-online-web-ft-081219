ea36bbb9a0bc43d25ab57d2f6fffc83c7c7a7481
Eric Loudon <loudon.eric@gmail.com>
2019-08-12 11:50:56 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot("Squawk!")
-puts
-end
+def parrot(string="Squawk!")
+puts "#{string}"
 
-parrot
+"#{string}"
+
+end
